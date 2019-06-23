c84ca18846443b917c296a47232b39457424797d
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-06-23 20:13:40 +0300

    получаем позицию элемента и по этой позиции выводим данные из массива ...

--- app/src/main/java/com/omelchenkoaleks/colordescriptor/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/colordescriptor/MainActivity.java
@@ -3,12 +3,43 @@ package com.omelchenkoaleks.colordescriptor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // нужно получить индекс или позицию, выбранного пользователем элемента
+    private Spinner mColorsSpinner;
+
+    // нужно записать описание в этот элемент
+    private TextView mTempDescriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mColorsSpinner = findViewById(R.id.colors_spinner);
+        mTempDescriptionTextView = findViewById(R.id.description_temp_text_view);
+    }
+
+    public void showDescription(View view) {
+        // сначала нужно получить индекс выбранного элемента
+        int position = mColorsSpinner.getSelectedItemPosition();
+
+        /* вызываем созданный метод, который возвращает описание по позиции =
+            в этой переменной
+                уже есть описание характера пользователя
+         */
+        String description = getDescriptionByPosition(position);
+
+        // теперь заполняем TextView нужным описанием (данными)
+        mTempDescriptionTextView.setText(description);
+    }
+
+    private String getDescriptionByPosition(int position) {
+        // получаем ресурсы по их имени (string_array) - т.е. здесь массив с данными
+        String[] descriptions = getResources().getStringArray(R.array.description_of_temp);
+        return descriptions[position];
     }
 }
